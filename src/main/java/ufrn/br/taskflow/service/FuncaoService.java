25751bf7a7835d136fa451dae7340427ca28d750
ricardo <ricardo2ca@gmail.com>
2023-09-05 19:35:55 -0300

    Alterado para service generico

--- src/main/java/ufrn/br/taskflow/service/FuncaoService.java
+++ src/main/java/ufrn/br/taskflow/service/FuncaoService.java
@@ -1,7 +1,10 @@
 package ufrn.br.taskflow.service;
 
 import org.springframework.stereotype.Service;
+import ufrn.br.taskflow.core.base.BaseService;
+import ufrn.br.taskflow.model.Funcao;
+import ufrn.br.taskflow.repository.FuncaoRepository;
 
 @Service
-public class FuncaoService {
+public class FuncaoService extends BaseService<Funcao, FuncaoRepository> {
 }
--- src/main/java/ufrn/br/taskflow/service/UsuarioService.java
+++ src/main/java/ufrn/br/taskflow/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package ufrn.br.taskflow.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import ufrn.br.taskflow.core.base.BaseService;
 import ufrn.br.taskflow.model.Usuario;
 import ufrn.br.taskflow.repository.UsuarioRepository;
 
@@ -11,62 +13,18 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UsuarioService {
-    UsuarioRepository repository;
-    @Autowired
-    public UsuarioService(UsuarioRepository repository){
-        this.repository = repository;
-    }
+public class UsuarioService extends BaseService<Usuario, UsuarioRepository> {
 
-    public List<Usuario> listAll(){
-        return repository.findAll();
-    }
+    @Transactional
+    public Usuario patchUpdate(Usuario model) {
+        Optional<Usuario> modelDb = repository.findById(model.getId());
 
-    public Usuario create(Usuario u){
-        return repository.save(u);
-    }
-
-    public Usuario getById(Long id) {
-        Optional<Usuario> u = repository.findById(id);
-
-        if (u.isPresent()) {
-            return u.get();
+        if (modelDb.isPresent()) {
+            Usuario modelPart = modelDb.get();
+            modelPart.partialUpdate(model);
+            return repository.save(modelPart);
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado");
         }
     }
-
-    public Usuario update(Usuario u){
-        Optional<Usuario> buscado = repository.findById(u.getId());
-
-        if (buscado.isPresent()) {
-            return repository.save(u);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado");
-        }
-    }
-
-    public void delete(Long id){
-        Optional<Usuario> buscado = repository.findById(id);
-
-        if(buscado.isPresent()){
-            repository.deleteById(id);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado");
-        }
-    }
-
-    public Usuario patchUpdate(Usuario u){
-        Optional<Usuario> buscado = repository.findById(u.getId());
-
-        if(buscado.isPresent()){
-            Usuario busca = buscado.get();
-            busca.partialUpdate(u);
-            return repository.save(busca);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado");
-        }
-    }
-
-
 }
